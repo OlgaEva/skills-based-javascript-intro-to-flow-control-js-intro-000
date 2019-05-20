7f296f7f1071857b3a218f68bfb5bb935cfba072
OlgaEva <olgaeva@gmail.com>
2019-05-20 09:16:11 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,17 +1,30 @@
 function basicTeenager(age) {
-
+  if (12 < age < 20) {
+    return "You are a teenager!";
+  }
 }
 
 function teenager(age) {
-
+  if (12 < age < 20) {
+    return "You are a teenager!";
+  } else {
+    return "You are not a teenager";
+  }
 }
 
 function ageChecker(age) {
-
+  if (12 < age < 20) {
+    return "You are a teenager!";
+  } else if (age <= 12) {
+    return "You are a kid";
+  } else {
+    return "You are a grownup";
+  }
 }
 
 function ternaryTeenager(age) {
-
+  12 < age < 20 ?
+  "You are a teenager" : "You are not a teenager";
 }
 
 function switchAge(age) {
